9754f6857e8a16206c09d14b7dc1698f51cad8e6
mironalisherovich1-cpu <miron.alisherovich1@icloud.com>
2025-12-26 22:56:30 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+START_IMAGE_URL = os.getenv("START_IMAGE_URL")
+
+ADMIN_USERNAME = "qwerty7777jass"
+
+START_BALANCE = 5.0
+
+CITIES = ["Bukhara", "Navai", "Samarkand", "Tashkent"]
+
+DEFAULT_LANG = "ru"
+DEFAULT_CITY = "Bukhara"
